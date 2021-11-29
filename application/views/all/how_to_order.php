1d435daf231a376b382cbeb773dd93d523a85d74
rikyfaturozi <rikyfatur@gmail.com>
2021-11-29 17:58:29 +0700

    update view buy plant, dan how to order (ayuni)

---
+++ application/views/all/how_to_order.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">  
+    <title>How To Order</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/order.css">
+</head>
+<body>
+    <nav>
+        <div class="awal" > 
+            <img src="<?php echo base_url()?>assets/img/logo.png" alt="">
+            <div class="nama">
+                StartPlant
+            </div>
+        </div>
+        <div class="tengah">
+            <a class="menu" href="<?php echo base_url().'index.php/home'?>">HOME</a>
+            <a class="menu" href="<?php echo base_url().'index.php/home/guide'?>">PLANT CARE GUIDE </a>
+            <a class="menu" href="<?php echo base_url().'index.php/home/how_to_order'?>">HOW TO ORDER</a>
+            <a class="menu" href="<?php echo base_url().'index.php/home/aboutus'?>">ABOUT US</a>
+
+        </div>
+        <div class="akhir">
+            <a class="login" href="<?php echo base_url(). 'index.php/home/login'?>"> <img src="<?php echo base_url()?>assets/img/login.png"></a>
+            <a class="buy" href="<?php echo base_url().'index.php/home/buy_plant'?>"><img src="<?php echo base_url()?>assets/img/carbon_shopping-cart.png"> Buy Plant Giude</a>
+        </div>
+    </nav>
+        <h2>How To Order StartPlant?</h2>
+        <div class = "top">
+            <div class = "img">
+                <img src="<?php echo base_url()?>assets/img/image53.png">
+            </div>
+            <div class = "hto">
+                <ol>
+                    <li>Buka website starplant dengan memasukkan link www.starplantofficial.com</li>
+                    <li>Pilih kategori kemudian klik tambahkan ke keranjang</li>
+                    <li>Melakukan konfirmasi pembayaran</li>
+                    <li>File PDF sudah bisa didowload</li>
+                </ol>
+            </div>
+        </div>
+        <div class = "buttom">
+            <table class = "cc">
+                <tr>
+                    <th>Frequently Asked Questions</th>
+                </tr>
+                <tr>
+                    <td>10 ways to raise the humidity in your house<tr></td>
+                    <td>How to apply fertilizer correctly?<tr></td>
+                    <td>How to grow chili plants?<tr></td>
+                    <td>How do you start a vegetable garden?<tr></td>
+                    <td>Trees  - Watering and winter care<tr></td>
+                    <td>Where can you get garden design inspiration?</td>
+                </tr>
+            </table>
+            <table class = "cp">
+                <tr>
+                    <th>Choose Payment</th>
+                </tr>
+                <tr>
+                    <td>Transfer Bank<tr></td>
+                    <td>Alfamart / Alfamidi<tr></td>
+                    <td>Indomaret<tr></td>
+                    <td>Gopay / Ovo<tr></td>
+                    <td>Dana<tr></td>
+                </tr>
+            </table>
+            <div class = "img2">
+                <img src="<?php echo base_url()?>assets/img/Vector21.png">
+            </div>
+        </div>
+</body>
+<footer>
+    <div class="ft">
+        <img src="<?php echo base_url()?>assets/img/vector 2.png">
+        <h4>StartPlant</h4>
+    </div>
+    <div class="xx">
+        <h3>Now, Everyone can grow their own plant</h3>
+    </div>
+    <div class ="grs1"></div>
+    <div class="vv">
+        <h4>Articel</h4>
+        <p>Email Marketing Campaigns</p>
+    </div>
+    <div class="vs">    
+        <h4>About</h4>
+        <p> Our Story Benefits</p>
+    </div>    
+    <div class="lk">
+        <h4>Follow Us</h4>
+        <a class="xx" href ="https://www.instagram.com/rikyfaturozii/"><img src="<?php echo base_url()?>assets/img/fb.png"></a>
+        <a class="xx" href ="https://www.instagram.com/rikyfaturozii/"><img src="<?php echo base_url()?>assets/img/ig.png"></a>
+    </div> 
+    <p class = "oo">Copyright © 2021. StarPlant BY Kelompok 4</p>
+</footer>
+</html>
+        
